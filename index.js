955e8d3d8b51eaff2d117d7b110a0bb5669c559c
lelin171046 <lelin171046@gmail.com>
2024-07-14 13:03:07 +0600

    add update section

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -32,23 +32,79 @@ async function run() {
 
     const userCollection = client.db('userDB').collection('user');
     const dataCollection = client.db('dataDB').collection('data');
-
-    app.post('/users', async(req, res)=>{
-        const user = req.body;
-        console.log(user);
-        const result = await userCollection.insertOne(user);
-        console.log(result);
-        res.send(result)
+    //uplaoadin user data
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      console.log(result);
+      res.send(result)
     })
 
     ///add iteam data
-    app.post('/additem', async(req, res)=>{
-        console.log(req.body);
-        const item = req.body;
-        const result = await dataCollection.insertOne(item)
-        res.send(result)
+    app.post('/additem', async (req, res) => {
+      console.log(req.body);
+      const item = req.body;
+      const result = await dataCollection.insertOne(item)
+      res.send(result)
+    })
+    //my craft list
+
+    app.get('/myCraft/:email', async (req, res) => {
+      const result = await dataCollection.find({ email: req.params.email }).toArray();
+      res.send(result)
+
     })
 
+    //all craft loaded
+    app.get('/allCraft', async (req, res) => {
+      const allCraft = await dataCollection.find().toArray();
+      res.send(allCraft)
+    })
+    //  update data
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updateItem = req.body;
+      console.log(updateItem);
+      // const item = {
+      //   $set: {
+      //     name: updateItem.name,
+      //     creator: updateItem.creator,
+      //     description: updateItem.description,
+      //     publishDate: updateItem.publishDate,
+      //     imageUrl: updateItem.imageUrl,
+      //     category: updateItem.category,
+      //     price: updateItem.price,
+      //     rating: updateItem.rating,
+      //     customization: updateItem.customization,
+      //     stockStatus: updateItem.stockStatus,
+
+      //   }
+      // };
+      // const result = await dataCollection.updateOne(filter, item, option);
+      // res.send(result)
+    })
+    //delete craft from my card
+    app.delete('/myCraft/:id', async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: new ObjectId(id) };
+      console.log(quary);
+      const result = await dataCollection.deleteOne(quary);
+      res.send(result)
+    })
+
+
+    ///Craft details find with id
+
+    app.get('/allCraft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const result = await dataCollection.findOne(query);
+      res.send(result);
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
